0d3f7fd853fc91c56783ad7d68e2ce7171d6ebdc
Simone Boccolari <simone.boccolari@hotmail.it>
2020-08-08 17:42:46 +0200

    const refactoring

--- hello.go
+++ hello.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello "
+
 // Hello function MUST start with a CAPITAL LETTER if you want to export the function
 func Hello(name string) string {
-	return "Hello " + name
+	return englishHelloPrefix + name
 }
 
 func main() {
