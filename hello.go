370318cf1dbc519475bd8a0f461432dcf97e0466
Simone Boccolari <simone.boccolari@hotmail.it>
2020-08-09 17:12:42 +0200

    new test

--- hello.go
+++ hello.go
@@ -6,6 +6,9 @@ const englishHelloPrefix = "Hello "
 
 // Hello function MUST start with a CAPITAL LETTER if you want to export the function
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPrefix + name
 }
 
--- hello_test.go
+++ hello_test.go
@@ -8,11 +8,23 @@ import "testing"
 the function takes only one argument t *testing.T
 t of the type *testing.T is like a "hook" to the testing library */
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello Chris"
 
-	if got != want {
-		// %q wraps the value in a double quoted string
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello World' when an empty strin is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
+		assertCorrectMessage(t, got, want)
+	})
 }
