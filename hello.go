fd8d42e652595291633cd63c8e0a71fa267c03cb
Simone Boccolari <simone.boccolari@hotmail.it>
2020-08-09 17:42:32 +0200

    spanish and french

--- hello.go
+++ hello.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello "
+const spanishHolaPrefix = "Hola "
+const frenchBonjourPrefix = "Bonjour "
 
 // Hello function MUST start with a CAPITAL LETTER if you want to export the function
-func Hello(name string) string {
+func Hello(name string, lang string) string {
+	if lang == french {
+		return frenchBonjourPrefix + name
+	}
+	if lang == spanish {
+		return spanishHolaPrefix + name
+	}
 	if name == "" {
 		name = "World"
 	}
@@ -13,5 +23,5 @@ func Hello(name string) string {
 }
 
 func main() {
-	fmt.Println(Hello("Go"))
+	fmt.Println(Hello("Go", "Spanish"))
 }
