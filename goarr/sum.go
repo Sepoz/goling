81bfc1fe8a3542067881c8455e56e2e457613f38
Simone Boccolari <simone.boccolari@hotmail.it>
2020-08-10 15:59:49 +0200

    arrays and slices

--- goarr/sum.go
+++ goarr/sum.go
@@ -2,10 +2,40 @@ package arrays
 
 // Sum function
 func Sum(numbers []int) int {
+
 	sum := 0
 
 	for _, value := range numbers {
 		sum += value
 	}
 	return sum
+}
+
+// SumAllTails function
+func SumAllTails(numbersToSum ...[]int) []int {
+
+	var sums []int
+	for _, value := range numbersToSum {
+
+		if len(value) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := value[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
+
+// Multiply function
+func Multiply(numbers []int) int {
+
+	mult := 1
+
+	for _, value := range numbers {
+		mult = mult * value
+	}
+
+	return mult
 }
